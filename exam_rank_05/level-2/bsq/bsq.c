89e06f5d9463d9fab449efa010303d4d70fad800
biertisch <almeidarodrigues.beatriz@gmail.com>
2026-02-14 00:22:52 +0000

    minor adjustments

--- exam_rank_05/level-2/bsq/bsq.c
+++ exam_rank_05/level-2/bsq/bsq.c
@@ -1,36 +1,21 @@
 #include "bsq.h"
 
-static void free_matrix(int** matrix, int height)
+static int ft_isprint(char c)
 {
-	if (!matrix)
-		return;
-
-	for (int i = 0; i < height; i++)
-		free(matrix[i]);
-	free(matrix);
-}
-
-static void free_string_array(char** array)
-{
-	if (!array)
-		return;
-
-	for(int i = 0; array[i]; i++)
-		free(array[i]);
-	free(array);
+	return c >= 32 && c <= 126;
 }
 
 static char* ft_strdup(char* src, int len)
 {
-	if (!src || len < 1)
+	if (!src)
 		return NULL;
 
-	char* dest = calloc(len + 1, sizeof(char));
+	char* dest = malloc(len + 1);
 	if (!dest)
 		return NULL;
 
-	int i;
-	for (i = 0; i < len && src[i]; i++)
+	int i = 0;
+	for (; i < len && src[i]; i++)
 		dest[i] = src[i];
 
 	dest[i] = '\0';
@@ -47,62 +32,75 @@ static int ft_min(int a, int b, int c)
 	return min;
 }
 
-static int validate_line(char* line, t_map* map)
+static void free_map(char** grid)
 {
-	if (!line || !map)
-		return 0;
+	if (!grid)
+		return;
 
-	for (int i = 0; line[i]; i++)
-	{
-		if (line[i] != map->empty && line[i] != map->obstacle)
-			return 0;
-	}
-	return 1;
+	for (int i = 0; grid[i]; i++)
+		free(grid[i]);
+	free(grid);
 }
 
-static int validate_parameters(FILE* file, t_map* map)
+static void free_matrix(int** array, int size)
+{
+	if (!array)
+		return;
+
+	for (int i = 0; i < size && array[i]; i++)
+		free(array[i]);
+	free(array);
+}
+
+static int parse_parameters(FILE* file, t_map* map)
 {
 	if (fscanf(file, "%d %c %c %c", &map->height, &map->empty, &map->obstacle, &map->full) != 4)
 		return 0;
 
 	if (map->height < 1)
 		return 0;
-	if (map->empty < 32 || map->empty > 126)
-		return 0;
-	if (map->obstacle < 32 || map->obstacle > 126)
-		return 0;
-	if (map->full < 32 || map->full > 126)
+	if (!ft_isprint(map->empty) || !ft_isprint(map->obstacle) || !ft_isprint(map->full))
 		return 0;
 	if (map->empty == map->obstacle || map->empty == map->full || map->obstacle == map->full)
 		return 0;
 
+	char* line = NULL;
+	size_t len = 0;
+	if (getline(&line, &len, file) != 1 || !line || line[0] != '\n')
+	{
+		free(line);
+		return 0;
+	}
+	free(line);
 	return 1;
 }
 
-static int validate_map(FILE* file, t_map* map)
+static int validate_row(char* row, int width, t_map* map)
 {
-	if (!file || !map || !validate_parameters(file, map))
+	if (!row || !map)
 		return 0;
 
+	if (width != map->width)
+		return 0;
+
+	for (int i = 0; row[i]; i++)
+		if (row[i] != map->empty && row[i] != map->obstacle)
+			return 0;
+
+	return 1;
+}
+
+static int parse_rows(FILE* file, t_map* map)
+{
 	char* line = NULL;
 	size_t len = 0;
-	if (getline(&line, &len, file) == -1 || !line || line[0] != '\n')
-		return 0;
-
-	map->grid = calloc(map->height + 1, sizeof(char*));
-	if (!map->grid)
-		return 0;
-
 	for (int i = 0; i < map->height; i++)
 	{
-		free(line);
-		line = NULL;
-		len = 0;
 		int read = getline(&line, &len, file);
 		if (read < 1)
 		{
 			free(line);
-			free_string_array(map->grid);
+			free_map(map->grid);
 			return 0;
 		}
 
@@ -113,37 +111,55 @@ static int validate_map(FILE* file, t_map* map)
 		if (!map->grid[i])
 		{
 			free(line);
-			free_string_array(map->grid);
+			free_map(map->grid);
 			return 0;
 		}
 
 		if (i == 0)
 			map->width = read;
-		if (read != map->width || !validate_line(map->grid[i], map))
+		if (!validate_row(map->grid[i], read, map))
 		{
 			free(line);
-			free_string_array(map->grid);
+			free_map(map->grid);
 			return 0;
 		}
+		free(line);
+		line = NULL;
+		len = 0;
 	}
-
-	free(line);
 	return 1;
+}
+
+static int parse_map(FILE* file, t_map* map)
+{
+	if (!file || !map)
+		return 0;
+
+	if (!parse_parameters(file, map))
+		return 0;
+
+	map->grid = calloc(map->height + 1, sizeof(char*));
+	if (!map->grid)
+		return 0;
+
+	return parse_rows(file, map);
 }
 
 static int find_biggest_square(t_map* map)
 {
-	map->bsq.size = 0;
-	int** matrix = malloc(map->height * sizeof(int*));
+	int** matrix = calloc(map->height, sizeof(int*));
 	if (!matrix)
 		return 0;
 
+	map->bsq.x = 0;
+	map->bsq.y = 0;
+	map->bsq.size = 0;
 	for (int i = 0; i < map->height; i++)
 	{
-		matrix[i] = malloc(map->width * sizeof(int));
+		matrix[i] = calloc(map->width, sizeof(int));
 		if (!matrix[i])
 		{
-			free_matrix(matrix, map->height);
+			free_matrix(matrix, i);
 			return 0;
 		}
 		for (int j = 0; j < map->width; j++)
@@ -154,16 +170,16 @@ static int find_biggest_square(t_map* map)
 				matrix[i][j] = 1;
 			else
 				matrix[i][j] = 1 + ft_min(
-					matrix[i - 1][j - 1],
 					matrix[i - 1][j],
-					matrix[i][j - 1]
+					matrix[i][j - 1],
+					matrix[i - 1][j - 1]
 				);
 
 			if (matrix[i][j] > map->bsq.size)
 			{
+				map->bsq.x = i - matrix[i][j] + 1;
+				map->bsq.y = j - matrix[i][j] + 1;
 				map->bsq.size = matrix[i][j];
-				map->bsq.x = i - map->bsq.size + 1;
-				map->bsq.y = j - map->bsq.size + 1;
 			}
 		}
 	}
@@ -172,8 +188,11 @@ static int find_biggest_square(t_map* map)
 	return 1;
 }
 
-void print_map(t_map* map)
+static void print_map(t_map* map)
 {
+	if (!map)
+		return;
+
 	for (int i = 0; i < map->height; i++)
 	{
 		for (int j = 0; j < map->width; j++)
@@ -188,7 +207,7 @@ void print_map(t_map* map)
 	}
 }
 
-int bsq(FILE* file)
+static int bsq(FILE* file)
 {
 	if (!file)
 	{
@@ -197,7 +216,7 @@ int bsq(FILE* file)
 	}
 
 	t_map map;
-	if (!validate_map(file, &map))
+	if (!parse_map(file, &map))
 	{
 		printf("map error\n");
 		return 1;
@@ -205,21 +224,20 @@ int bsq(FILE* file)
 
 	if (!find_biggest_square(&map))
 	{
+		free_map(map.grid);
 		printf("memory allocation failed\n");
 		return 1;
-	};
+	}
+
 	print_map(&map);
-	free_string_array(map.grid);
+	free_map(map.grid);
 	return 0;
 }
 
 int main(int argc, char** argv)
 {
 	if (argc > 2)
-	{
-		printf("invalid number of arguments\n");
 		return 1;
-	}
 
 	if (argc == 1)
 		return bsq(stdin);
@@ -231,9 +249,6 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	int ret = bsq(file);
-	if (ret)
-		printf("map error\n");
 	fclose(file);
-
 	return ret;
 }
